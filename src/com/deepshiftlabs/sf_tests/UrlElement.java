ab6dedb5a77aee9565b5181aaaf0f433f481c21e
Dima Yakubovsky <bear@deepshiftlabs.com>
2009-06-14 10:23:51 +0000

    Is URL element displayed as link check corrected.

--- src/com/deepshiftlabs/sf_tests/UrlElement.java
+++ src/com/deepshiftlabs/sf_tests/UrlElement.java
@@ -10,7 +10,7 @@ public class UrlElement extends GenericTextElement {
         		values.get(i).shouldBeDisplayed="http://"+values.get(i).shouldBeDisplayed;
         }
         
-        readLocator = "//*[@class='labelCol' and text()='"+a_elementName+"']/following::a";
+//        readLocator = "//*[@class='labelCol' and text()='"+a_elementName+"']/following::a";
         	
         values.add(new CheckValue("http:/adr<e>ss.com", Constants.IT_IS_VALID_VALUE, "", "http://http:/adress.com"));        
         values.add(new CheckValue("http://domain", Constants.IT_IS_VALID_VALUE, "", "http://domain"));
@@ -33,7 +33,7 @@ public class UrlElement extends GenericTextElement {
     		return Constants.RET_PAGE_BROKEN_OK;
     	} else {
     		errorsCount++;
-    		action.closeEventError(event);
+    		action.closeEventError(event, "Element is not displayed as link!");
     		return Constants.RET_PAGE_BROKEN_ERROR;
     	}
     }
